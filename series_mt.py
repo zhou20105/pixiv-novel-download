f4e39a7ef8dfcc375a35c44531dbb621b5455e89
zhou20105 <100961128+zhou20105@users.noreply.github.com>
2025-01-09 15:36:24 +0800

    Update series_mt.py

--- series_mt.py
+++ series_mt.py
@@ -130,30 +130,7 @@ def process_chapter(novel_url, chapter_number, series_folder, cookies):
 
 def download_series(series_id, status_label, progress_bar, output_label, output_path):
     cookies = [
-    {"name": "first_visit_datetime_pc", "value": "2024-11-02%2016%3A36%3A23", "domain": ".pixiv.net", "path": "/"},
-    {"name": "p_ab_id", "value": "7", "domain": ".pixiv.net", "path": "/"},
-    {"name": "p_ab_id_2", "value": "2", "domain": ".pixiv.net", "path": "/"},
-    {"name": "p_ab_d_id", "value": "1405298390", "domain": ".pixiv.net", "path": "/"},
-    {"name": "yuid_b", "value": "NymXFBA", "domain": ".pixiv.net", "path": "/"},
-    {"name": "PHPSESSID", "value": "84620081_AOAUJkysGvAa7wCOzTVu1jGnr5QWlyl8", "domain": ".pixiv.net", "path": "/"},
-    {"name": "device_token", "value": "29ab9c0bc4d87ec35b628138a077a952", "domain": ".pixiv.net", "path": "/"},
-    {"name": "privacy_policy_agreement", "value": "7", "domain": ".pixiv.net", "path": "/"},
-    {"name": "c_type", "value": "25", "domain": ".pixiv.net", "path": "/"},
-    {"name": "privacy_policy_notification", "value": "0", "domain": ".pixiv.net", "path": "/"},
-    {"name": "a_type", "value": "0", "domain": ".pixiv.net", "path": "/"},
-    {"name": "b_type", "value": "1", "domain": ".pixiv.net", "path": "/"},
-    {"name": "__utmc", "value": "235335808", "domain": ".pixiv.net", "path": "/"},
-    {"name": "__utmz", "value": "235335808.1731673314.3.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)", "domain": ".pixiv.net", "path": "/"},
-    {"name": "_gid", "value": "GA1.2.1140275552.1731673323", "domain": ".pixiv.net", "path": "/"},
-    {"name": "_gcl_au", "value": "1.1.933459697.1731674003", "domain": ".pixiv.net", "path": "/"},
-    {"name": "login_ever", "value": "yes", "domain": ".pixiv.net", "path": "/"},
-    {"name": "FCNEC", "value": "%5B%5B%22AKsRol85ClY3AwfyMj54zcarYk00RxgcOaHHrozjnRMz5OSG9wMNsUSmXGw0NBfObg792NaeOIP13maOdW2qCHdeqzstLyzW8-3WrWNlZZ4FJKaeuBHiP7O9CGnI11fJQCny8zRdGgAf2CJbBHMFt3N_TDNPUgQ71g%3D%3D%22%5D%5D", "domain": ".pixiv.net", "path": "/"},
-    {"name": "__utmv", "value": "235335808.|2=login%20ever=yes=1^3=plan=normal=1^5=gender=male=1^6=user_id=84620081=1^9=p_ab_id=7=1^10=p_ab_id_2=2=1^11=lang=zh=1", "domain": ".pixiv.net", "path": "/"},
-    {"name": "__utma", "value": "235335808.1902634853.1730532986.1731682059.1731732021.6", "domain": ".pixiv.net", "path": "/"},
-    {"name": "cf_clearance", "value": "bGaFX3BsTgxjfPvTG.KcsogLRRXxDmWjzhMMiNoA8po-1731732811-1.2.1.1-u___Vba3qPKSajPGYWNsiAq9uhJBEfOpS_wXr.bvHwZgYn4XcYj1NtinsFlA7weuNx0PeOqWy3cq5XbF7NRSZdSuLtqKEWa09IY7GrCkLyoj06qo6Nem3UFSEX0myY.CFzcZw8NphWe4bV90MtcoD7Ha4wZw5INzyncqyV.BZJzvAULVO9YNdkkTjAA64SqVDDjRhNJSsakRHA6Clls4zNXwsWoH1omyHXmrNC67harP8bNNUdSFWTNSpfFZi6yBrIBch1_UjJJniIpaXw5VncwBnSmPjoV_Aqd6QYSx36cFzHCZw.Gwv0FfSmA9I8yFTnv5qhM14Th28qBLq5ASjQkLcXxs7NGZ14IUWtf0iQ0GneRVKQPM4z52dwYv5Xkk", "domain": ".pixiv.net", "path": "/"},
-    {"name": "_ga", "value": "GA1.2.1902634853.1730532986", "domain": ".pixiv.net", "path": "/"},
-    {"name": "_ga_75BBYNYN9J", "value": "GS1.1.1731747352.9.1.1731747549.0.0.0", "domain": ".pixiv.net", "path": "/"},
-    {"name": "__cf_bm", "value": "SqhvjjOCJhsBElKZXZrEcjcmU_UF.5QQMey61hTt.B0-1731762715-1.0.1.1-oAWABDh8hoL78xlGMC3AMElms8uG.x5dfm6PbwWvit1a0fNvYfJMSQ_3EiuEfMpFzNUNUxoLjz0fJxiEl0E.cwq3FH9PzJlmsyoYjWWbiwA", "domain": ".pixiv.net", "path": "/"}
+    ........ #替换你自己的cookie
     ]
     
     status_label.config(text="Downloading...")
